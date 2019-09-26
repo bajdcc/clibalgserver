7d1fbb3e199becde6ba7d07862ad7fbcc6fab292
bajdcc <bajdcc@foxmail.com>
2019-09-26 18:55:18 +0800

    ADD: Add focus trans

--- clibalgserver/server/example/test_1d.cpp
+++ clibalgserver/server/example/test_1d.cpp
@@ -1,21 +1,24 @@
 #include "/include/io"
 #include "/include/math"
 #include "/include/trace"
+int tmp;
 void swap(int* a, int* b) {
-    int t = *a;
+    tmp = *a;
     *a = *b;
-    *b = t;
+    *b = tmp;
 }
 int main(int argc, char** argv) {
     int a[10], i, j;
 	int n = sizeof(a) / sizeof(int);
     trace_log("一维数组修改示例");
-    trace_array_1d("a", &a, T_INT, n);
+    trace_var("临时变量", &tmp, T_INT);
+    trace_array_1d("数组", &a, T_INT, n);
     for (i = 0; i < n; i++)
         a[i] = rand() % 9 + 1;
     for (i = 0; i < n - 1; i++)
         for (j = 0; j < n - i - 1; j++)
             if (a[j] > a[j + 1])
                 swap(&(a[j]), &(a[j + 1]));
-    trace_end("a");
+    trace_end("数组");
+    trace_end("临时变量");
 }
--- clibalgserver/server/example/test_2d.cpp
+++ clibalgserver/server/example/test_2d.cpp
@@ -5,8 +5,8 @@ int main(int argc, char** argv) {
     int a[3][4], i;
 	int n = sizeof(a) / sizeof(int);
     trace_log("二维数组修改示例");
-    trace_array_2d("a", &a, T_INT, 3, 4);
+    trace_array_2d("数组", &a, T_INT, 3, 4);
     for (i = 0; i < n; i++)
         ((int*)&a)[i] = i;
-    trace_end("a");
+    trace_end("数组");
 }
